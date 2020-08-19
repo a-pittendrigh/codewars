20912087c9dd3b21ac74cfb20512480481ad54ba
Alexander Pittendrigh <alex@simply.co.za>
2020-08-19 22:18:40 +0200

    final solution

--- src/bookseller/core.clj
+++ src/bookseller/core.clj
@@ -1,67 +1,41 @@
 (ns bookseller.core)
 ;; kata https://www.codewars.com/kata/54dc6f5a224c26032800005c/train/clojure
 (defn stock-list [list-of-books list-of-cat]
-  (->> list-of-books
-       (into (map (fn [category] (str category " 0")) list-of-cat))
-       (map #(clojure.string/split % #" "))
-       (group-by #(first (first %)))
-       (mapv (fn [[cat books]]
-               [(str cat)
-                (reduce (fn [sum [_ qty]] (+ sum (Integer/parseInt qty))) 0 books)]))
-       (sort-by first))
-  )
-
-(let [ur ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"]
-      vr ["A" "B" "C" "D"]
-      res [["A" 0] ["B" 1290] ["C" 515] ["D" 600]]]
-  (= (stock-list ur vr) res)
-  #_(stock-list ur vr))
-
-(comment
-  (into (map
-         (fn [category] (str category " 0"))
-         ["A" "B" "C" "D"]) ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"])
-
-
-  (type \A)
-  (= "A" (str \A))
-  (str \A)
-
-  (-> 1 (-> identity))
-  (-> "1 1"(->> #" " (clojure.string/split)))
-  (-> "1 1" (clojure.string/split #" "))
-  (reduce (fn [k v] (prn v) (into k v)) {:a [] :b []})
-  (prn "sadaskl")
-
-  (reduce
-   (fn [a b]
-     (prn (second b)
-          b)
-     (into a (second b)))
-   ;;#(+ %1 (second (Integer/parseInt %2)))
-   []
-   (parseInt "1")
-   )
-  
-  (->> [["BBAR" "150"] ["BKWR" "250"] ["BTSQ" "890"]]
-   (map second)
-   (map #(Integer/parseInt %))
-   (reduce +))
-  (reduce )
-
-  (juxt (fn [t] t) {:a [1 2 3] :b [1 2 3]})
-  )
-
-
-
-;; (ns bookseller.core-test
-;;   (:require [clojure.test :refer :all]
-;;             [bookseller.core :refer :all]))
+  (if (or (= [] list-of-books) (= [] list-of-cat))
+      []
+      (let [wanted-category-totals (set list-of-cat)
+            initial-category-totals (mapv (fn [category] (str category " 0")) list-of-cat)
+            ordered-category-totals (into {} (map #(clojure.string/split % #" ") initial-category-totals))]
+        (->> list-of-books
+             (into initial-category-totals)
+             (map #(clojure.string/split % #" "))
+             (group-by #(first (first %)))
+             (mapv (fn [[cat books]]
+                     [(str cat)
+                      (reduce (fn [sum [_ qty]] (+ sum (Integer/parseInt qty))) 0 books)]))
+             (filter (fn [[cat _]] (contains? wanted-category-totals cat)))
+             (into {})
+             (merge ordered-category-totals)
+             (into [])))))
 
 (comment
-  (deftest a-test1
-    (testing "Test 1"
-      (def ur ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"])
-      (def vr ["A" "B" "C" "D"])
-      (def res [["A" 0] ["B" 1290] ["C" 515] ["D" 600]])
-      (is (= (stock-list ur vr) res)))))
+  (let [ur ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600" "W 500"]
+        vr [ "B" "C" "D" "Z" "A"]
+        vr []
+        res [["B" 1290] ["C" 515] ["D" 600] ["A" 0]]]
+    #_(= (stock-list ur vr) res)
+    #_(->>
+     (stock-list ur vr)
+     (into {})
+     (merge {"A" 10, "B" 1290, "C" 515, "D" 600, "Z" 0}))
+
+    ;;
+    (= [] [])
+    (stock-list ur vr)
+    )
+
+         ( ->>
+          (into {} [["U" 0] ["V" 0] ["R" 225]])
+          (into []))
+         )
+
